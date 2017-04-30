8bcc581cd2c1c5e9b1be5d2b717de0f10e5221d3
FM <ekkalakfm@gmail.com>
2017-05-01 02:49:30 +0700

    remove

--- src/reducers/reducer_book.js
+++
@@ -1,8 +0,0 @@
-export default function () {
-  return [
-    {title: "Focus", pages: 58},
-    {title: "FM", pages: 85},
-    {title: "Kuy", pages: 1412},
-    {title: "KUYYYYY", pages: 123}
-  ];
-}
